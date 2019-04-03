958db50019435e203a473de889b13a1ee70da075
Anton <anton.mois33v@gmail.com>
2019-04-03 15:07:25 -0400

    Implemented a better way

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function removeOddSum(list, key1, key2) {
-  const evenSums = [];
-  list.forEach((item) => {
-    const sum = item[key1] + item[key2];
-    if (sum % 2 === 0) {
-      evenSums.push(item);
-    }
-  });
-  return evenSums;
+  // const evenSums = [];
+  // list.forEach((item) => {
+  //   const sum = item[key1] + item[key2];
+  //   if (sum % 2 === 0) {
+  //     evenSums.push(item);
+  //   }
+  // });
+  // return evenSums;
+  return list.filter(item => (item[key1] + item[key2]) % 2 === 0);
 }
 
 module.exports = removeOddSum;
