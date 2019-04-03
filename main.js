6eb03ff298f8ad785357261b5555c7ea5c2680d5
Anton <anton.mois33v@gmail.com>
2019-04-03 12:16:07 -0400

    Exercise complete

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function removeOddSum() {
+function removeOddSum(list, key1, key2) {
+  const evenSums = [];
+  list.forEach((item) => {
+    const sum = item[key1] + item[key2];
+    if (sum % 2 === 0) {
+      evenSums.push(item);
+    }
+  });
+  return evenSums;
 }
 
 module.exports = removeOddSum;
